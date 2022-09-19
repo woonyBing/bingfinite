7664d427646f3e47dcd70fa03231bdc5bf68f4be
woonyBing <honeybye@naver.com>
2022-09-19 17:57:44 +0900

    Navbar 고정 완료 / 메인 영역 레이아웃 잡기 완료

--- src/App.js
+++ src/App.js
@@ -3,33 +3,50 @@ import "./App.css";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import { Routes, Route, useNavigate } from "react-router-dom";
 
+
 function App() {
   let navigate = useNavigate();
 
   return (
     <div>
-      <div className="mainBanner">
-        <div className="Header">
+      <div className="Header">
           <Navbar className="navbar">
             <Container>
               <Navbar.Brand id="navbarLogo" href="/">Navbar</Navbar.Brand>
               <Nav>
-                <Nav.Link className="navbarCategory" href="#home">About Me</Nav.Link>
-                <Nav.Link className="navbarCategory" href="#pricing">Project</Nav.Link>
-                <Nav.Link className="navbarCategory" href="#features">Skill</Nav.Link>
-                <Nav.Link className="navbarCategory" href="#pricing">Contact</Nav.Link>
+                <Nav.Link className="navbarCategory" onClick={()=>{navigate('/aboutme')}}>About Me</Nav.Link>
+                <Nav.Link className="navbarCategory" onClick={()=>{navigate('/project')}}>Project</Nav.Link>
+                <Nav.Link className="navbarCategory" onClick={()=>{navigate('/skill')}}>Skill</Nav.Link>
+                <Nav.Link className="navbarCategory" onClick={()=>{navigate('/contact')}}>Contact</Nav.Link>
               </Nav>
             </Container>
           </Navbar>
-          <Routes>
-            <Route path="/" element={<div>메인</div>}></Route>
-            <Route path="/aboutme" element={<div>About Me</div>}></Route>
-            <Route path="/Project" element={<div>Project</div>}></Route>
-            <Route path="/skill" element={<div>Skill</div>}></Route>
-            <Route path="/contact" element={<div>Contact</div>}></Route>
-          </Routes>
-        </div>
+          <div className="mainBanner">
+            
+          </div>
       </div>
+
+      <Routes>
+        <Route path="/" element={
+          <div>
+            <div className="AboutMe">
+              <h1 className="AboutMeTitle">AboutMe</h1>
+            </div>
+            <div className="Project">
+              <h1 className="ProjectTitle">Project</h1>
+            </div>
+            <div className="Contact">
+              <h1 className="ContactTitle">Contact</h1>
+            </div>
+            <div className="Footer"></div>
+          </div>
+        }></Route>
+        <Route path="/aboutme" element={<div>About Me</div>}></Route>
+        <Route path="/project" element={<div>Project</div>}></Route>
+        <Route path="/skill" element={<div>Skill</div>}></Route>
+        <Route path="/contact" element={<div>Contact</div>}></Route>
+      </Routes>
+      
     </div>
   );
 }
