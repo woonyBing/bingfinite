7c37334184dc409d41dfdf20b9f592200fe8bf54
woonyBing <honeybye@naver.com>
2022-09-22 16:44:49 +0900

    about me 페이지 완성

--- src/component/AboutMe/AboutMe.js
+++ src/component/AboutMe/AboutMe.js
@@ -16,15 +16,19 @@ function AboutMe() {
             <div className="AboutBody">
                 <Information/>
                 <Career/>
-
-
                 <div className="Skill" style={{backgroundColor:'rgb(248, 248, 248)', paddingTop:'80px', paddingBottom:'80px'}}>
                     <h5 style={{color:'rgb(73, 132, 232)'}}>Skill</h5>
                     <Skill/>
                 </div>
-
-
-
+                <div>
+                    <h5 className="contact">Contact</h5>
+                    <div  className="contactContainer">
+                    <div className="contactButton">
+                        <img src={process.env.PUBLIC_URL + 'images/email.png'}/>E-mail</div>
+                    <div className="contactButton"><img src={process.env.PUBLIC_URL + 'images/call.png'}/>Call</div>
+                    <div className="contactButton"><img src={process.env.PUBLIC_URL + 'images/kakao.png'}/>KaKao Talk</div>
+                    </div>
+                </div>
             </div>
         </div>
     )
