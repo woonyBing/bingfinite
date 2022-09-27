db1c3902187bd5de1398ae35c786ad89d91a9f5a
woonyBing <honeybye@naver.com>
2022-09-28 03:04:53 +0900

    CopyToClipboard 활용하여 클립보드 복사, alert 해결

--- src/component/AboutMe/AboutMe.js
+++ src/component/AboutMe/AboutMe.js
@@ -91,49 +91,6 @@ function AboutMe() {
                 </div>
             </div>
 
-
-            {/* 
-            <div className="AboutHeader">
-                <div className="AboutTop">
-                    <h3>About Me</h3>
-                    <div style={{color:'gray'}}>안녕하세요 신입 개발자 빙예은입니다</div>
-                    <div className={"abTabInfoFrom " + abTabInfoTo} onClick={()=>{
-                        setAbTabInfoTo('abTabInfoTo');
-                        setAbTabInfoTo2('');
-                        setAbTabInfoTo3('');
-                        setAbTabInfoTo4('');
-                        navigate('/aboutme/#Information');
-                        document.getElementById('Information').scrollIntoView({behavior:'smooth'});
-                    }}>Information</div>
-                    <div className={"abTabInfoFrom2 " + abTabInfoTo2} onClick={()=>{
-                        setAbTabInfoTo2('abTabInfoTo2');
-                        setAbTabInfoTo('');
-                        setAbTabInfoTo3('');
-                        setAbTabInfoTo4('');
-                        navigate('/aboutme/#WorkCareer');
-                        document.getElementById('WorkCareer').scrollIntoView({behavior:'smooth'});
-                    }}>Work Career</div>
-                    <div className={"abTabInfoFrom3 " + abTabInfoTo3} onClick={()=>{
-                        setAbTabInfoTo3('abTabInfoTo3');
-                        setAbTabInfoTo('');
-                        setAbTabInfoTo2('');
-                        setAbTabInfoTo4('');
-                        navigate('/aboutme/#Skill')
-                        document.getElementById('Skill').scrollIntoView({behavior:'smooth'});
-                    }}>Skill</div>
-                    <div className={"abTabInfoFrom4 " + abTabInfoTo4} onClick={()=>{
-                        setAbTabInfoTo4('abTabInfoTo4');
-                        setAbTabInfoTo('');
-                        setAbTabInfoTo2('');
-                        setAbTabInfoTo3('');
-                        navigate('/aboutme/#Contact')
-                        document.getElementById('Contact').scrollIntoView({behavior:'smooth'});
-                    }}>Contact</div>
-                </div>
-            </div>
-             */}
-
-
             
             <div className="AboutBody">
                 <div>
--- src/component/AboutMe/Contact.js
+++ src/component/AboutMe/Contact.js
@@ -1,14 +1,30 @@
+import { useState, useRef } from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+
 function Contact() {
+    // alert('E-mail이 복사되었습니다');
+
+    let email = 'abc12@naver.com';
+    let phoneNumber = '010-1234-5678';
+
     return(
         <div className="contactContainer">
             <h5 className="contact">Contact</h5>
             <div className="contactBox">
-                {/* 누르면 메일, 전화번호, 카카오톡 복사되는 거 넣기 */}
-                <div className="contactButton">
-                    <img src={process.env.PUBLIC_URL + '/images/email.png'}/>E-mail</div>
-                <div className="contactButton">
-                    <img src={process.env.PUBLIC_URL + '/images/call.png'}/>Call</div>
-                <div className="contactButton">
+                <CopyToClipboard text={email} onCopy={()=>alert('주소가 복사되었습니다. ( ' + email + ' )' )}>
+                    <div className="contactButton">
+                        <img src={process.env.PUBLIC_URL + '/images/email.png'}/>E-mail
+                    </div>
+                </CopyToClipboard>
+                
+                <CopyToClipboard text={email} onCopy={()=>alert('번호가 복사되었습니다. ( ' + phoneNumber + ' )' )}>
+                    <div className="contactButton">
+                        <img src={process.env.PUBLIC_URL + '/images/call.png'}/>Call
+                    </div>
+                </CopyToClipboard>
+                <div className="contactButton" onClick={()=>{
+                    window.open('https://open.kakao.com/o/sqpvBcEe')
+                }}>
                     <img src={process.env.PUBLIC_URL + '/images/kakao.png'}/>KaKao Talk</div>
             </div>
         </div>
